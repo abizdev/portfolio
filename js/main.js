6ad75237df2a6787ce7f7ee118beff8d325d95fa
Abdulaziz <abizlancer@gmail.com>
2023-07-16 17:32:15 +0500

    feat: add fontawesome and menu

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ if(ScrollTrigger.isTouch !== 1) {
 gsap.fromTo('.header--title span', {y: '110%'}, {y: 0, ease: "Power4.easeOut", duration: 2 }, '<')
 gsap.fromTo('.header__sub--title span', {y: '110%'}, {y: 0, ease: "Power4.easeOut", duration: 2 }, '<')
 // gsap.fromTo('.header__sub--title span', {y: '110%'}, {y: 0, ease: "Power4.easeOut", duration: 2 }, '<')
+
+const menu = document.querySelector('.menu')
+const menuOpenBtn = document.querySelector('.navbar__burger')
+const menuCloseBtn = document.querySelector('.menu__close')
+
+menuOpenBtn.addEventListener('click', () => { 
+  menu.classList.add('active')
+  // gsap.to(menu, {h: '100vh', duration: 2, ease: "Power4.easeOut"})
+})
+menuCloseBtn.addEventListener('click', () => { 
+  // gsap.to(menu, {h: '0', duration: 2, ease: "Power4.easeOut"})
+  // setTimeout(() => {
+  //   menu.removeAttribute('style')
+  // }, 2000);
+  menu.classList.remove('active') 
+})

d7eccf9b563d950ec8d0f0f4fa50de2ebd4d5257
Abdulaziz <abizlancer@gmail.com>
2023-07-30 13:27:20 +0500

    feat: animation to about section

--- js/main.js
+++ js/main.js
@@ -26,12 +26,13 @@ menuCloseBtn.addEventListener('click', () => {
 })
 
 // about section
-// const tlAbout = gsap.timeline({
-//   ScrollTrigger:{
-//     trigger: '.about',
-//     start: '-40%',
-//     end: '0%',
-//     marker: true
-//   }
-// })
-gsap.fromTo('.about__content--title span', {y: '110%'}, {y: 0, ease: "Power4.easeOut", duration: 2 })
+const tlAbout = gsap.timeline({
+  scrollTrigger:{
+    trigger: '.about',
+    start: '-70%',
+    end: '0%',
+  }
+})
+tlAbout.fromTo('.about--bg', { opacity: 0 }, { opacity: 1, ease: "Power4.easeOut", duration: 2.35 }, )
+tlAbout.fromTo('.about__content--title span', {y: '110%'}, {y: 0, ease: "Power4.easeOut", duration: 1.75 }, '<20%')
+tlAbout.fromTo('.about__content--info span', { opacity: 0,}, { opacity: 1, ease: "Power4.easeOut", duration: 1.75 }, '<')
